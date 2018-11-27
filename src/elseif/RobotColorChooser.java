9ba7d749eefcab5831bef7519ac07b00f430a881
League <league@WTS9.attlocal.net>
2018-11-26 16:57:24 -0800

    i nsdf,mnldkznv didd oijkoasjflk;aj stuf;lkngklzgv.

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,12 +4,15 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot rob=new Robot();
 		//3. Ask the user what color they would like the robot to draw
 		
 		//5. Use an if/else statement to set the pen color that the user requested
