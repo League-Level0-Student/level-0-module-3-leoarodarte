45c5610e726389c658ac5fd43e14097fac1bdb6e
League <league@WTS9.attlocal.net>
2019-01-07 17:01:24 -0800

    b

---
+++ sketch_190107a/sketch_190107a.pde
@@ -0,0 +1,11 @@
+void setup(){
+for(int i=99; i>0; i--){
+  println( i +" bottles of beer on the wall!!!");
+  println( i +" bottles of beer on the wall. "+ i + " bottles of beer!! Take one down, pass it around, ");
+  
+}
+}
+void draw(){
+  
+
+}
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -11,9 +11,14 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+rob.setX(200);
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+rob.hide();
+rob.penDown();
+rob.setSpeed(50);
+for(int i=0; i<5; i++) {
+rob.turn(144);
+rob.move(30);}
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
