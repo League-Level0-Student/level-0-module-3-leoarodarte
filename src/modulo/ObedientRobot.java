98f261c8b0c2acabe6d393b91f0437b374a997b7
League <league@WTS9.attlocal.net>
2019-02-04 16:54:11 -0800

    I made a robot.
    It can draw different colored shapes of my choice.

--- src/modulo/ObedientRobot.java
+++ src/modulo/ObedientRobot.java
@@ -1,14 +1,45 @@
 package modulo;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-public static void main(String[] args) {
-	Robot rob=new Robot();
-	rob.penDown();
-	rob.setSpeed(5);
-	rob.setPenWidth(10);
-	rob.setPenColor(109, 70, 19);
+	static Robot rob=new Robot();
+	public static void main(String[] args) {
+		rob.penDown();
+		rob.setSpeed(5);
+		rob.setPenWidth(10);
+		rob.hide();
+		String s = JOptionPane.showInputDialog("What do you want me to draw? Square, triangle, or a circle.");
+		String c = JOptionPane.showInputDialog("Waht color do you want? Red, Green, Yellow or Blue.");
+		if(c.equals("Red.")) {
+			rob.setPenColor(224, 13, 13);
+		}
+		if(c.equals("Blue.")) {
+			rob.setPenColor(13,13,224);
+		}
+		if(c.equals("Yellow.")) {
+			rob.setPenColor(255, 229, 0);
+		
+		}
+		if(c.equals("Green.")) {
+			rob.setPenColor(13,224,13);
+		}
+		if(s.equals("Square.")) {
+			drawSquare();
+		}
+		else if(s.equals("Triangle.")) {
+			drawTriangle();
+		}
+		else if(s.equals("Circle.")) {
+			drawCircle();
+		}
+
+		}
+	
+public static void drawSquare() {
+	
 	rob.move(200);
 	rob.turn(90);
 	rob.move(200);
@@ -16,5 +47,20 @@ public static void main(String[] args) {
 	rob.move(200);
 	rob.turn(90);
 	rob.move(200);
+	
+}
+public static void drawTriangle() {
+	rob.turn(-90);
+rob.move(300);
+rob.turn(120);
+rob.move(300);
+rob.turn(120);
+rob.move(300);
+}
+public static void drawCircle() {
+	for(int i=0; i<360; i++) {
+		rob.move(2);
+		rob.turn(1);
+	}
 }
 }
